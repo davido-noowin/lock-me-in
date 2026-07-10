a0ed6b85596a90e4865679649a2cbbd848684296
David Nguyen <davido.nguyen1212@gmail.com>
2026-07-10 12:25:57 -0700

    modal confirmation to start timer + warning

--- src/components/AlertModal.tsx
+++ src/components/AlertModal.tsx
@@ -1,7 +1,43 @@
-export default function AlertModal() {
-    return (
-        <div>
-            this is the modal
-        </div>
-    )
+import {
+  Description,
+  Dialog,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+import type { Dispatch, SetStateAction } from "react";
+
+type ModalProps = {
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
+  setIsRunning: Dispatch<SetStateAction<boolean>>;
+};
+
+export default function AlertModal(props: ModalProps) {
+    function CloseModal() {
+        props.setIsOpen(false);
+    }
+
+    function StartTimer() {
+        props.setIsRunning(true);
+        CloseModal();
+    }
+
+  return (
+    <Dialog open={props.isOpen} onClose={() => props.setIsOpen(false)}>
+      <div>
+        <DialogPanel>
+          <DialogTitle>Notice:</DialogTitle>
+          <Description>
+            While the timer is active, the sites below will all be blocked and
+            you cannot access them until the timer runs out.
+          </Description>
+          <p>Are you sure you want to proceed?</p>
+          <div>
+            <button onClick={CloseModal}>Cancel</button>
+            <button onClick={StartTimer}>Start Timer</button>
+          </div>
+        </DialogPanel>
+      </div>
+    </Dialog>
+  );
 }
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -2,10 +2,12 @@ import "../styles/components/Timer.css";
 import playLightMode from "../assets/images/playLightMode.png";
 import playDarkMode from "../assets/images/playDarkMode.png";
 import { useState, useEffect } from "react";
+import AlertModal from "./AlertModal";
 
 export default function Timer(props: { mode: string }) {
   const [time, setTime] = useState(0); // Time in seconds
   const [isRunning, setIsRunning] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     if (isRunning && time > 0) {
@@ -28,9 +30,12 @@ export default function Timer(props: { mode: string }) {
 
   function handlePlayButtonClick() {
     console.log("Play button clicked");
+    if (time <= 0 ) {
+      return;
+    }
     // create a modal that warns the user they cant cancel the timer + it disables the switches
     // disable the play button
-    setIsRunning(true);
+    setIsOpen(true);
   }
 
   function formatTime(seconds: number): string {
@@ -47,36 +52,47 @@ export default function Timer(props: { mode: string }) {
 
   return (
     <div className="timer-container">
+      {isOpen && (
+        <AlertModal
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          setIsRunning={setIsRunning}
+        />
+      )}
       <div className="timer-display">
         <span className="timer-value">{formatTime(time)}</span>
       </div>
       <div className="timer-controls">
         <button
+          disabled={isRunning}
           className="timer-control-button"
           onClick={() => changeTime(-5 * 60)}
         >
           - 5 min
         </button>
         <button
+          disabled={isRunning}
           className="timer-control-button"
           onClick={() => changeTime(-1 * 60)}
         >
           - 1 min
         </button>
         <button
+          disabled={isRunning}
           className="timer-control-button"
           onClick={() => changeTime(1 * 60)}
         >
           + 1 min
         </button>
         <button
+          disabled={isRunning}
           className="timer-control-button"
           onClick={() => changeTime(5 * 60)}
         >
           + 5 min
         </button>
       </div>
-      <button className="timer-play-button" onClick={handlePlayButtonClick}>
+      <button disabled={isRunning} className="timer-play-button" onClick={handlePlayButtonClick}>
         <img
           src={props.mode === "light" ? playLightMode : playDarkMode}
           alt="Play Button"
